4b2c5c6a16c2a4e05232d5f526431efd1b7d38a0
Nighty3098 <154594695+Nighty3098@users.noreply.github.com>
2024-08-27 19:16:57 +0700

    Fix

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 import os
 import time
 import psutil
-from flask import Flask, jsonify, render_template, request, send_file
+from flask import Flask, jsonify, render_template, redirect
 from loguru import logger
-from get_info import (fetch_cpu_info, get_ip_address, get_service_name,
-                      get_uptime, gpu_info, model_info, os_name)
-
+from get_info import (fetch_cpu_info, get_ip_address, get_uptime, gpu_info, model_info, os_name)
 import platform, subprocess
 
 home_dir = os.path.expanduser("~")
@@ -21,8 +19,7 @@ logger.add(
 
 app = Flask(__name__)
 
-prev_bytes_recv = psutil.net_io_counters().bytes_recv
-prev_bytes_sent = psutil.net_io_counters().bytes_sent
+prev_net_io = psutil.net_io_counters()
 prev_time = time.time()
 
 @app.route("/")
@@ -31,7 +28,7 @@ def index():
 
 @app.route("/usage")
 def usage():
-    global prev_bytes_recv, prev_bytes_sent, prev_time
+    global prev_net_io, prev_time
 
     cpu_usage = psutil.cpu_percent(interval=1)
     ram_usage = psutil.virtual_memory().percent
@@ -41,11 +38,10 @@ def usage():
     current_time = time.time()
     elapsed_time = current_time - prev_time
 
-    bytes_recv = net_io.bytes_recv - prev_bytes_recv
-    bytes_sent = net_io.bytes_sent - prev_bytes_sent
+    bytes_recv = net_io.bytes_recv - prev_net_io.bytes_recv
+    bytes_sent = net_io.bytes_sent - prev_net_io.bytes_sent
 
-    prev_bytes_recv = net_io.bytes_recv
-    prev_bytes_sent = net_io.bytes_sent
+    prev_net_io = net_io
     prev_time = current_time
 
     speed_recv = bytes_recv / elapsed_time / 1024  # kB/s
@@ -58,18 +54,16 @@ def usage():
     logger.debug(f"Upload Speed: {speed_sent:.2f} kB/s")
 
     return jsonify(
-        {
-            "cpu_usage": cpu_usage,
-            "ram_usage": ram_usage,
-            "ram_total": psutil.virtual_memory().total,
-            "ram_used": psutil.virtual_memory().used,
-            "disk_usage": disk_usage,
-            "disk_total": psutil.disk_usage("/").total,
-            "disk_used": psutil.disk_usage("/").used,
-            "net_recv": speed_recv,
-            "net_sent": speed_sent,
-            "cpu_freq": psutil.cpu_freq().current
-        }
+        cpu_usage=cpu_usage,
+        ram_usage=ram_usage,
+        ram_total=psutil.virtual_memory().total,
+        ram_used=psutil.virtual_memory().used,
+        disk_usage=disk_usage,
+        disk_total=psutil.disk_usage("/").total,
+        disk_used=psutil.disk_usage("/").used,
+        net_recv=speed_recv,
+        net_sent=speed_sent,
+        cpu_freq=psutil.cpu_freq().current
     )
 
 @app.route("/system_info")
@@ -85,15 +79,13 @@ def get_info():
     logger.info(f"Device Name: {device_name}")
 
     return jsonify(
-        {
-            "device_uptime": device_uptime,
-            "device_ip": device_ip,
-            "sys_platform": os_name(),
-            "device_name": device_name,
-            "sys_model": model_info(),
-            "sys_cpu": fetch_cpu_info(),
-            "sys_gpu": gpu_info(),
-        }
+        device_uptime=device_uptime,
+        device_ip=device_ip,
+        sys_platform=os_name(),
+        device_name=device_name,
+        sys_model=model_info(),
+        sys_cpu=fetch_cpu_info(),
+        sys_gpu=gpu_info()
     )
 
 @app.route("/cpu_temp")
@@ -101,53 +93,19 @@ def cpu_temp():
     try:
         cpu_temp = psutil.sensors_temperatures()["coretemp"][0].current
         logger.debug(f"CPU Temperature: {cpu_temp:.2f}°C")
-        return jsonify({"cpu_temp": cpu_temp})
+        return jsonify(cpu_temp=cpu_temp)
     except Exception as e:
         logger.error(f"Error getting CPU temperature: {e}")
-        return jsonify({"cpu_temp": "N/A", "error": str(e)})
-
-@app.route('/files')
-def files():
-    log_directory = os.path.expanduser('~/logs/')
-    files = os.listdir(log_directory) if os.path.exists(log_directory) else []
-    return jsonify(files)
-
-@app.route('/download', methods=['POST'])
-def download():
-    selected_file = request.form['file']
-    file_path = os.path.join(os.path.expanduser('~/logs/'), selected_file)
-    
-    if os.path.isfile(file_path):
-        return send_file(file_path, as_attachment=True)
-    else:
-        return "File not found", 404
-
-@app.route('/open_file', methods=['POST'])
-def open_file():
-    selected_file = request.json['file']
-    file_path = os.path.join(os.path.expanduser('~/logs/'), selected_file)
-
-    if os.path.isfile(file_path):
-        with open(file_path, 'r') as f:
-            content = f.read()
-        return jsonify({'content': content})
-    else:
-        return jsonify({'error': 'File not found'}), 404
+        return jsonify(cpu_temp="N/A", error=str(e))
 
 @app.route('/shutdown', methods=['POST'])
 def shutdown():
-    if platform.system() == "Windows":
-        subprocess.call(["shutdown", "/s", "/t", "1"])
-    elif platform.system() == "Linux":
-        subprocess.call(["shutdown", "now"])
+    subprocess.call(["shutdown", "/s" if platform.system() == "Windows" else "now"])
     return redirect('/')
 
 @app.route('/reboot', methods=['POST'])
 def reboot():
-    if platform.system() == "Windows":
-        subprocess.call(["shutdown", "/r", "/t", "1"])
-    elif platform.system() == "Linux":
-        subprocess.call(["reboot"])
+    subprocess.call(["shutdown", "/r" if platform.system() == "Windows" else "reboot"])
     return redirect('/')
 
 @app.route('/sleep', methods=['POST'])
@@ -160,4 +118,4 @@ def sleep():
 
 if __name__ == "__main__":
     logger.info("Starting Flask application...")
-    app.run(debug=False, host=get_ip_address())
+    app.run(debug=True, host=get_ip_address())
--- get_info.py
+++ get_info.py
@@ -7,17 +7,14 @@ from subprocess import DEVNULL, PIPE, Popen
 
 import psutil
 
-
 def truncate_string(s, max_length):
     return s if len(s) <= max_length else f"{s[:max_length - 3]}..."
-
 
 def get_service_name(port):
     try:
         return socket.getservbyport(port)
     except OSError:
         return "Unknown Service"
-
 
 def get_uptime():
     if platform.system() != "Linux":
@@ -26,52 +23,32 @@ def get_uptime():
     with open("/proc/uptime", "r") as f:
         uptime_seconds = float(f.readline().split()[0])
 
-    days, remainder = divmod(uptime_seconds, 86400)
-    hours, remainder = divmod(remainder, 3600)
-    minutes, seconds = divmod(remainder, 60)
-
-    return f"{int(days)} d. {int(hours)} h. {int(minutes)} m. {int(seconds)} s."
-
+    return f"{uptime_seconds // 86400} d. {uptime_seconds % 86400 // 3600} h. {uptime_seconds % 3600 // 60} m. {uptime_seconds % 60:.0f} s."
 
 def get_ip_address():
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
         s.connect(("8.8.8.8", 80))
         return s.getsockname()[0]
 
-
 def run_command(command):
     process = Popen(command, stdout=PIPE, universal_newlines=True, shell=True, stderr=DEVNULL)
-    stdout, _ = process.communicate()
-    return stdout.strip()
-
+    return process.communicate()[0].strip()
 
 def os_name():
     os_file = "/etc/os-release" if path.isfile("/etc/os-release") else "/bedrock/etc/os-release"
-    pretty_name = run_command(f"grep 'PRETTY_NAME' {os_file}").replace('PRETTY_NAME=', '').replace('"', '')
-    return pretty_name
-
+    return run_command(f"grep 'PRETTY_NAME' {os_file}").replace('PRETTY_NAME=', '').replace('"', '')
 
 def model_info():
-    product_info = ""
-    if path.exists("/sys/devices/virtual/dmi/id/product_name"):
-        with open("/sys/devices/virtual/dmi/id/product_name", "r") as f:
-            product_info = f.read().strip()
-
-    if path.exists("/sys/devices/virtual/dmi/id/product_version"):
-        version_info = run_command("cat /sys/devices/virtual/dmi/id/product_version")
-        product_info += f" {version_info.strip()}" if product_info else version_info.strip()
-
-    return product_info
-
+    product_info = run_command("cat /sys/devices/virtual/dmi/id/product_name").strip()
+    version_info = run_command("cat /sys/devices/virtual/dmi/id/product_version").strip()
+    return f"{product_info} {version_info}" if version_info else product_info
 
 def fetch_cpu_info():
     cpu_count = len(run_command("ls /sys/class/cpuid/ | sort").splitlines())
     cpu_info = run_command("grep 'model name' /proc/cpuinfo").split(":")[1].strip()
     cpu_max_freq = int(run_command("cat /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq")) / 1000
-
     cpu_freq_info = f"{cpu_max_freq:.3f}MHz" if cpu_max_freq < 1000 else f"{cpu_max_freq / 1000:.3f}GHz"
     return f"{cpu_info} ({cpu_count}) @ {cpu_freq_info}"
-
 
 def gpu_info():
     if sys.platform == "darwin":
