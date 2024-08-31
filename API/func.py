d71436ca333b820be74810385fb37a7ee98f31dd
Nighty3098 <154594695+Nighty3098@users.noreply.github.com>
2024-08-31 22:08:05 +0700

    Formatting

--- API/func.py
+++ API/func.py
@@ -1,11 +1,17 @@
 from subprocess import DEVNULL, PIPE, Popen
+
+
 def convert_seconds_to_hhmm(seconds):
     hours, minutes = divmod(seconds, 3600)
     return f"{hours:02}:{minutes // 60:02}"
 
+
 def truncate_string(s, max_length):
     return s if len(s) <= max_length else f"{s[:max_length - 3]}..."
 
+
 def run_command(command):
-    process = Popen(command, stdout=PIPE, universal_newlines=True, shell=True, stderr=DEVNULL)
+    process = Popen(
+        command, stdout=PIPE, universal_newlines=True, shell=True, stderr=DEVNULL
+    )
     return process.communicate()[0].strip()
